33923b8d52a3fa2424c75d0e1fcec9f85cc339a0
ouyjy <706545330@qq.com>
2020-11-11 10:44:34 +0800

    修复了内建控制器ROS使能激光雷达导致仿真死机问题

--- src/tricycle_ros_controller.cpp
+++ src/tricycle_ros_controller.cpp
@@ -1,7 +1,7 @@
 #include "tricycle_class.h"
 #include <unistd.h>
 int main(int argc, char **argv) {
-    cout<<"loard"<<endl;
+    cout<<"Tricyle: Load Controller"<<endl;
     ros::init(argc, argv, "tricycle_control_node");
     TricycleWebotsCtrl robot("/cmd_vel","/move_base_simple/goal");
     while (robot.SimOnce()!= -1) {
